a0ca83b6d4361a402101e450373ed0c463c78dea
D-XIII <hugo.benard@live.fr>
2022-06-27 17:11:23 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+const fs = require('fs');
+const path = require('path')
+
+fastify.get('/report/:id', async (request, reply)=>{
+    const id = request.params.id
+    console.log(id)
+    const stream = fs.createReadStream(path.resolve(`${id}/reports/report.json`))
+
+    reply.type('text/json').send(stream)
+    return reply
+})
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 8100 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
